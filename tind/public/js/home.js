f2e534b716f3919edf3f2ac6e7ac64878210c129
Leonard Witzel <witzel@post.harvard.edu>
2013-11-19 14:50:36 -0500

    aggrgate costs added.

--- tind/public/js/home.js
+++ tind/public/js/home.js
@@ -46,11 +46,110 @@ function redraw(){
     
     $(".tooltip-1").remove();
     $("#graph-1 .box svg").remove();
+    $("#graph-2 .box svg").remove();
     $("#graph-5 .box svg").remove();
     draw_pmpm();
-    draw_cumulative();   
+    draw_cumulative();
+    draw_costs();   
 }
 
+function draw_costs(){
+    var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width-40], .1);
+
+    var y = d3.scale.linear()
+        .rangeRound([height, 0]);
+    
+    var color = d3.scale.ordinal()
+        /* .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]); */
+        /* .range(['#3ea4bf','#F6BB33', '#49bf92', '#a084bf', '#FF6A13']) */
+        .range(colors);
+    
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+    
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(d3.format(".2s"));
+    
+    var svg = d3.select("#aggregate_costs").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    d3.csv("../public/data/stacked_costs.csv", function(error, data) {
+      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "State"; }));
+    
+      data.forEach(function(d) {
+        var y0 = 0;
+        d.ages = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+        d.total = d.ages[d.ages.length - 1].y1;
+      });
+    
+      data.sort(function(a, b) { return b.total - a.total; });
+    
+      x.domain(data.map(function(d) { return d.State; }));
+      y.domain([0, d3.max(data, function(d) { return d.total; })]);
+    
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+    
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("PMPM ($)");
+    
+      var state = svg.selectAll(".state")
+          .data(data)
+        .enter().append("g")
+          .attr("class", "g")
+          .attr("transform", function(d) { return "translate(" + x(d.State) + ",0)"; });
+    
+      state.selectAll("rect")
+          .data(function(d) { return d.ages; })
+        .enter().append("rect")
+          .attr("width", x.rangeBand()/2)
+          .attr("x", x.rangeBand()/2/2)
+          .attr("y", function(d) { return y(d.y1); })
+          .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+          .style("fill", function(d) { return color(d.name); })
+/*
+          .append("g")
+          .append("text")
+          .text(function(d) { console.log(d); return d; })
+*/;
+    
+      var legend = svg.selectAll(".legend")
+          .data(color.domain().slice().reverse())
+        .enter().append("g")
+          .attr("class", "legend")
+          .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+    
+      legend.append("rect")
+          .attr("x", width - 18)
+          .attr("width", 18)
+          .attr("height", 18)
+          .style("fill", color);
+    
+      legend.append("text")
+          .attr("x", width - 24)
+          .attr("y", 9)
+          .attr("dy", ".35em")
+          .style("text-anchor", "end")
+          .text(function(d) { return d; });
+    
+    });    
+}
 function draw_pmpm(){    
 
     var minDate = Date.today().clearTime().moveToFirstDayOfMonth().addMonths(-12);
@@ -370,7 +469,6 @@ function draw_cumulative(){
     
     });
 }
-
 function ToggleAlertsBox(){
     if($("#alerts-box").css("display") == "none"){
         $("#alerts-box").css("opacity","0.5");
