0f80fbd9bcc34cb49fdebcd56d97856dcb989635
Leonard Witzel <witzel@post.harvard.edu>
2013-12-03 14:33:05 -0500

    temporary fix for das.

--- data/views.py
+++ data/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
-from data.das import Das
-
-@login_required
-def index(request):
-    status = Das()
-    return render_to_response('data/index.html',{"status":status})
+# from data.das import Das
+# 
+# @login_required
+# def index(request):
+#     status = Das()
+#     return render_to_response('data/index.html',{"status":status})
