847b99c72d79a97df890421a683ff51600628525
Leonard Witzel <witzel@post.harvard.edu>
2013-12-03 14:35:10 -0500

    temporary fix for das.

--- data/urls.py
+++ data/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from data import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
     #url(r'^authenticate/', views.authenticate),
 )
