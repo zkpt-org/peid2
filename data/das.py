3519ce2483689bc7ab9f7b9268e05f8c99752cba
Leonard Witzel <witzel@post.harvard.edu>
2013-12-16 11:20:16 -0500

    another half-baked commit.

--- data/das.py
+++ data/das.py
@@ -52,12 +52,12 @@ class Das:
         return self.curl(self.VALIDATE, p)
 
     def get_proxy_granting_ticket(self, iou):
-        p = {"iou":iou}
-        ticket = self.curl(self.TICKET, p)
-        #if iou:
-        #    ticket = ProxyTicket.objects.filter(ticket_iou=iou)[0].ticket_id
-        #else:
-        #    ticket = ProxyTicket.objects.latest('created').ticket_id
+        #p = {"iou":iou}
+        #ticket = self.curl(self.TICKET, p)
+        if iou:
+           ticket = ProxyTicket.objects.filter(ticket_iou=iou)[0].ticket_id
+        else:
+           ticket = ProxyTicket.objects.latest('created').ticket_id
         return ticket
 
     def get_proxy_ticket(self, pgt):
