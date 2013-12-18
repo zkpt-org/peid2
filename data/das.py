ee4facd7d852d66b6336a924e7d9594de56bf55d
Leonard Witzel <witzel@post.harvard.edu>
2013-12-18 10:18:48 -0500

    debugging.

--- data/das.py
+++ data/das.py
@@ -9,7 +9,7 @@ class Das:
         self.SERVICE  = 'https://tind-lite.zakipoint.com'
         self.PROXY    = 'https://tind-staging.herokuapp.com/data/proxy'
         self.VALIDATE = 'https://login.deerwalk.com/cas/serviceValidate'
-        self.API_URL  = 'https://das.deerwalk.com:8443'
+        self.API_URL  = 'https://das.deerwalk.com'
         self.PT_URL   = 'https://login.deerwalk.com/cas/proxy'
         self.TICKET   = 'https://tind-staging.herokuapp.com/data/ticket/'
 
