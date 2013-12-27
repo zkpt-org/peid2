bcda9f2c6660104b55da7c6c95821e619b1734a6
Leonard Witzel <witzel@post.harvard.edu>
2013-12-27 10:50:55 -0500

    API testing traceback commit.

--- data/views.py
+++ data/views.py
@@ -27,6 +27,7 @@ def proxy(request):
         ticket.save()
         return render_to_response('data/index.html',{"status":"OK"})
     else:
+        django.core.mail.mail_admins("proxy error", str(request.GET), fail_silently=False)
         return render_to_response('data/index.html',{"status":"MISSING DATA"})
 
 #@login_required
