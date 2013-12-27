74282e64efdaff3e3750db0dca821d06527a1e4f
Leonard Witzel <witzel@post.harvard.edu>
2013-12-27 11:04:33 -0500

    API testing traceback commit.

--- tind/settings.py
+++ tind/settings.py
@@ -200,3 +200,6 @@ EMAIL_HOST=os.environ['EMAIL_HOST']
 EMAIL_PORT=os.environ['EMAIL_PORT']
 EMAIL_HOST_USER=os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD=os.environ['EMAIL_HOST_PASSWORD']
+
+SERVER_EMAIL='witzel@post.harvard.edu'
+DEFAULT_FROM_EMAIL='witzel@post.harvard.edu'
