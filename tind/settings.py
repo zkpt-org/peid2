9e19e1c3c34afad91b0e416b7ccee77cad220449
Leonard Witzel <witzel@post.harvard.edu>
2013-12-31 12:37:00 -0500

    modified logging.

--- tind/settings.py
+++ tind/settings.py
@@ -168,13 +168,21 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'strm': sys.stdout
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'data': {
+            'handlers': ['console'],
         },
     }
 }
