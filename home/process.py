7b26ec0b590967a7786519189b96683467880bc9
Leonard Witzel <witzel@post.harvard.edu>
2014-02-01 23:39:01 -0500

    fixed locale issue for heroku.

--- home/process.py
+++ home/process.py
@@ -100,7 +100,7 @@ def graph2(request):
     return data
 
 def graph3(request):
-    locale.setlocale(locale.LC_ALL, 'en_US')
+    locale.setlocale(locale.LC_ALL, 'en_US.utf8')
     das = Das()
     
     params = {
