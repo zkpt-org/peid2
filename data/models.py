30365079363e5355d5db0699aad01d58f71e209b
Leonard Witzel <witzel@post.harvard.edu>
2014-04-08 13:21:34 -0400

    cohort model added to data.

--- data/models.py
+++ data/models.py
@@ -12,3 +12,15 @@ class ProxyTicket(models.Model):
             ('ticket_iou', self.ticket_iou),
             ('ticket_id', self.ticket_id),
         ))
+        
+class Cohort(models.Model):
+    cohort_id = models.CharField(max_length=64)
+    traits    = models.TextField()
+    
+    def json(self):
+        from collections import OrderedDict
+        return OrderedDict((
+            ('id', self.id),
+            ('cohort_id', self.cohort_id),
+            ('traits', self.traits),
+        ))
